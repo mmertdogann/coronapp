2efd963ce3234ff5f0ab2fc56135f1d02232d1c2
mmertdogann <doganme@mef.edu.tr>
2020-08-30 15:17:16 +0300

    Configure styles

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  Card,
-  CardContent,
-  Typography,
-  Grid,
-  StylesProvider,
-} from '@material-ui/core';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
 import cx from 'classnames';
 
